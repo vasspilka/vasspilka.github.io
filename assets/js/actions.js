43fcb393569d7a2d711d45ba943a938c3237f2b3
Vasilis Spilka <vasspilka@gmail.com>
2015-04-23 16:49:05 +0300

    Automata to projects

---
+++ assets/js/actions.js
@@ -0,0 +1,7 @@
+$("#lg-card").click( function() {
+    window.location.href='https://github.com/vasspilka/LocalGrowth';
+})
+
+$("#automata-card").click( function() {
+    window.location.href='https://github.com/dionyziz/Automata';
+})
